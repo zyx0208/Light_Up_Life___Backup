cca9e3c4a49387dc7b08a4c9e19340c8b561d467
RebeRit <reberit0000@gmail.com>
2022-11-27 08:49:22 +0900

    PatchNote 1.9
    - Add Prologue Story background image (made AI images)

--- Assets/Resources/Code/StoryScriptWritter.cs
+++ Assets/Resources/Code/StoryScriptWritter.cs
@@ -9,13 +9,18 @@ public class StoryScriptWritter : MonoBehaviour
 {
     public Text scriptObj;
     List<string> scripts = new List<string>();
+    List<string> images = new List<string>();
     int checkScriptLine;
     public GameObject nextStoryBtn;
+    public GameObject backgroundImg;
+    public Image background;
     public Button nextBtn;
 
     // Start is called before the first frame update
     void Start()
     {
+        background = backgroundImg.GetComponent<Image>();
+
         scripts.Add("빛의 가호를 받으며 살아가는 '만다린 왕국'");
         scripts.Add("왕국은 빛을 사용한 마법으로 크게 번영하여 모두가 행복한 삶을 살았습니다.");
         scripts.Add("왕국은 빛의 마법을 담은 크리스탈을 왕국 곳곳에 두어 이 삶이 영원하길 바랐습니다.");
@@ -25,11 +30,21 @@ public class StoryScriptWritter : MonoBehaviour
         scripts.Add("모든 빛이 사라지고 세상은 어둠으로 뒤덮이게 되었습니다.");
         scripts.Add("그러나, 단 한 집에서 약한 불빛이 보이고 있었는데...");
 
+        images.Add("Images/Story_0_01_Background");
+        images.Add("Images/Story_0_02_Background");
+        images.Add("Images/Story_0_03_Background");
+        images.Add("Images/Story_0_04_Background");
+        images.Add("Images/Story_0_05_Background");
+        images.Add("Images/Story_0_06_Background");
+        images.Add("Images/Story_0_07_Background");
+        images.Add("Images/Story_0_08_Background");
+
         nextBtn.onClick.AddListener(nextScript);
 
         checkScriptLine = 0;
         scriptObj.text = "";
         StartCoroutine(Typing(scripts[checkScriptLine]));
+        background.GetComponent<Image>().sprite = Resources.Load<Sprite>(images[checkScriptLine].ToString());
         nextStoryBtn.SetActive(false);
         Invoke("showNextBtn", 4.0f);
     }
@@ -40,6 +55,7 @@ public class StoryScriptWritter : MonoBehaviour
             ++checkScriptLine;
             scriptObj.text = "";
             StartCoroutine(Typing(scripts[checkScriptLine]));
+            background.GetComponent<Image>().sprite = Resources.Load<Sprite>(images[checkScriptLine].ToString());
             Debug.Log("Show Next Script");
             nextStoryBtn.SetActive(false);
             Invoke("showNextBtn", 4.0f);
